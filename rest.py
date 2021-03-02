92c1ef7fc1a31bcb80695d97fae5f64b522c395b
Sai Shibu N B <saishibu38380@gmail.com>
2021-03-02 10:34:19 +0530

    Update rest.py

--- rest.py
+++ rest.py
@@ -103,7 +103,7 @@ def welcome():
 	security(str(sys._getframe().f_code.co_name))
 #	print "Welcome to Amrita Smart-Grid Middleware"
 #	print "kindly use one of the APIs to get data"
-	return "Welcome to Amrita Intelligent Infrastructure Data Management and Control Panel App. \n Use one of the options below."
+	return render_template('/home/saishibu/OceanNet-Webpage/index.html')
 	#return render_template('/home/cs/SGM/Server/welcome.html')
 
 @app.route('/stp/test')
